9972070c6591cef60d2fa3e5044d2da7a1016e34
liwenbo <liwenbo4922@163.com>
2022-12-19 16:31:18 +0800

    提交vue_test的todo案例

---
+++ src/components/MyList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="todo-main">
+    <MyItemVue v-for="todoObj in todos" :todo="todoObj" :key="todoObj.id"/>
+  </ul>
+</template>
+
+<script>
+import MyItemVue from "./MyItem.vue";
+export default {
+  name: "MyList",
+  components: {
+    MyItemVue,
+  },
+  props:['todos']
+};
+</script>
+
+<style scoped>
+
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/MyStudent2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生年龄：{{ age }}</h2>
+    <h2>学生年龄：{{ sex }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyStudent2",
+  data() {
+    console.log(this)
+    return {
+      msg:'我是学生'
+    };
+  },
+  // props:['name','age','sex']
+  props:{
+    name:String,
+    age:Number,
+    sex:String
+  }
+};
+</script>
